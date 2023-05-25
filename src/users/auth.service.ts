f60457142be34ddf8eec8065f923e678d1038486
Kordian Korsakowski <kordian.korsakowski@asterisk-dev.pl>
2023-05-25 12:22:35 +0200

    add authService

---
+++ src/users/auth.service.ts
@@ -0,0 +1,11 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+}
